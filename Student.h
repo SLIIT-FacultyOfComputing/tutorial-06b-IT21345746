3519279a59143ccc052e7d3faceaae3fc5734bad
Dananji  Paramitha <99312048+IT21345746@users.noreply.github.com>
2022-03-16 05:31:30 +0000

    Completed

--- Student.h
+++ Student.h
@@ -1,9 +1,14 @@
 class Student {
   // private section
+private:  
   //    int studentId
+int studentID;
   //    name <- 20 charcters
-  
-  // public section
+ char studentName[20]; 
+  // public 
+public:
   //    assignDetails() method declaration
+void assignDetails(int no,char name[]);
   //    display() method declaration
+void display();
 };
